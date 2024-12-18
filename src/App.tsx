5815b16ae350e1840441cc6d70e32aaa02cb0952
BarsatKhadka <khadkabarsat598@gmail.com>
2024-12-18 13:18:21 -0600

    update: The magic of zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { useUserStore } from "./store"
+
 function App() {
 
+  const userStore = useUserStore();
+  console.log(userStore)
   return (
     <>
+    <p>{userStore.username}</p>
     </>
   )
 }
--- src/store.tsx
+++ src/store.tsx
@@ -1,1 +1,18 @@
 import {create} from 'zustand'
+
+
+export interface useUserType{
+    username: string;
+    email: string;
+    setUsername : (username: string) => void;
+    setEmail : (email:string)=> void;
+}
+export const useUserStore =  create<useUserType>(
+    (set) =>({
+        username: "hello",
+        email : "",
+        setUsername: (username: string) => set(() => ({username})),
+        setEmail: (email: string) => set(() => ({email}) )
+
+    })
+);
